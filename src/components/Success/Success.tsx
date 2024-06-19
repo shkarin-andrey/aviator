709f22145f7232f63f2afeb77e01494fbf2d73ff
r09-Andrey <r09-andrey@monowork.pro>
2024-06-19 13:11:42 +0400

    fix: анимация звезд

--- src/components/Success/Success.tsx
+++ src/components/Success/Success.tsx
@@ -8,13 +8,13 @@ const Success: FC<ISuccess> = ({ winCount, reward }) => {
       <SuccessIcon className="absolute left-1/2 -translate-x-1/2 -top-[65px]" />
       <div className="flex flex-col items-start">
         <div className="text-[#60CFFF] font-bold text-[18px] leading-none ml-3">Win factor</div>
-        <div className="px-7 py-1 rounded-full text-[24px] font-bold text-[#228AED] bg-[#C2FDFF] leading-none">
+        <div className="px-7 py-1 rounded-full text-[24px] font-bold text-[#228AED] bg-[#C2FDFF] leading-none text-nowrap">
           {winCount} x
         </div>
       </div>
       <div className="flex flex-col items-end">
         <div className="text-[#60CFFF] font-bold text-[18px] leading-none mr-4">Reward</div>
-        <div className="px-7 py-1 rounded-full text-[24px] font-bold text-[#228AED] bg-[#C2FDFF] leading-none">
+        <div className="px-7 py-1 rounded-full text-[24px] font-bold text-[#228AED] bg-[#C2FDFF] leading-none text-nowrap">
           $ {reward}
         </div>
       </div>
